860a8b919d12c7098555b48ea60cabd93009f4f2
Hergoln <juliusz.malka@o2.pl>
2020-06-27 20:07:57 +0200

    refactor

--- LoadBalancerMTO/LoadBalancer.cs
+++ LoadBalancerMTO/LoadBalancer.cs
@@ -6,7 +6,7 @@
         {
             if(vms.Length > 0)
             {
-                servers[0].CurrentLoadPercentage = (double)vms[0].Size / servers[0].Capacity * 100.0d;
+                servers[0].AddVm(vms[0]);
             }
         }
     }
--- LoadBalancerMTO/Server.cs
+++ LoadBalancerMTO/Server.cs
@@ -1,13 +1,21 @@
-﻿namespace LoadBalancerMTO
+﻿using System;
+
+namespace LoadBalancerMTO
 {
     public class Server
     {
         public int Capacity;
         public double CurrentLoadPercentage;
+        private double MAXIMUM_LOAD = 100.0d;
 
         public bool Contains(Vm theVm)
         {
             return true;
+        }
+
+        public void AddVm(Vm vm)
+        {
+            this.CurrentLoadPercentage = (double)vm.Size / this.Capacity * MAXIMUM_LOAD;
         }
     }
 }
