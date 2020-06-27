cac8c80050578dd458e92c1d92b07f8ba682e246
Hergoln <juliusz.malka@o2.pl>
2020-06-27 15:00:00 +0200

    Base for tests in XUnit with NHamcrest

---
+++ NUnitTestProject/LoadBalancerMTOTests.cs
@@ -0,0 +1,19 @@
+using Xunit;
+using NHamcrest;
+using NHamcrest.XUnit;
+
+namespace NUnitTestProject
+{
+    public class LoadBalancerMTOTests
+    {
+        public LoadBalancerMTOTests()
+        {
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
